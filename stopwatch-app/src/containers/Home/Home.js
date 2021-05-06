7fc509f4bc9ab1ad4260379d37883ff231c75812
Andrey Leonets <awdenheimlee@gmail.com>
2021-05-06 21:10:08 +0300

    Create Home container

---
+++ stopwatch-app/src/containers/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Home = () => {
+  return (
+    <>
+      <div>HH:MM:SS</div>
+      <div>
+        <div>Start/Stop</div>
+        <div>Wait</div>
+        <div>Reset</div>
+      </div>
+    </>
+  );
+};
