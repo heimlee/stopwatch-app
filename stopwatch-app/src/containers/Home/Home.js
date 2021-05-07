41d0dcb4696798e8bc91be557034e8f86c81ca0d
Andrey Leonets <awdenheimlee@gmail.com>
2021-05-07 05:24:47 +0300

    Release App using React

--- stopwatch-app/src/containers/Home/Home.js
+++ stopwatch-app/src/containers/Home/Home.js
@@ -1,5 +1,5 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import { fromEvent, Observable } from 'rxjs';
+import React, { Fragment, useRef, useState } from 'react';
+// import { Observable } from 'rxjs';
 
 import { Timer } from '../../components/Timer/Timer';
 import { Button } from '../../components/UI/Button/Button';
@@ -8,39 +8,48 @@ import './home.scss';
 
 export const Home = () => {
   const [ sec, setSec ] = useState(0);
-  const [ isStart, setIsStart ] = useState(false);
+  const [ isStarted, setIsStarted ] = useState(false);
+  const [ isPaused, setIsPaused ] = useState(false);
+  const countRef = useRef(null);
 
-  useEffect(() => {
-    const observable = new Observable(observer => {
-      setInterval(() => {
-        observer.next(setSec(s => s + 1000))
-      }, 1000);
-    });
-
-    observable.subscribe();
-  }, []);
 
   const start = () => {
+    setIsStarted(true);
+    setIsPaused(true);
+    countRef.current = setInterval(() => {
+      setSec((s) => s + 1000);
+    }, 1000);
   };
 
   const stop = () => {
+    clearInterval(countRef.current);
+    setIsStarted(false);
+    setIsPaused(true);
+    setSec(0);
   };
 
   const wait = () => {
+    setIsStarted(false);
+    setIsPaused(true);
+    clearInterval(countRef.current);
   };
 
   const reset = () => {
+    clearInterval(countRef.current);
+    setIsStarted(false);
+    setIsPaused(false);
+    setSec(0);
   };
 
   return (
     <Fragment>
       <Timer sec={sec} />
       <div className="buttons-row">
-        {!isStart ? 
+        {!isStarted ? 
           <Button id="startBtn" onClick={start}>Start</Button> : 
           <Button onClick={stop}>Stop</Button>
         }
-        <Button onClick={wait}>Wait</Button>
+        <Button onDoubleClick={wait}>Wait</Button>
         <Button onClick={reset}>Reset</Button>
       </div>
     </Fragment>
